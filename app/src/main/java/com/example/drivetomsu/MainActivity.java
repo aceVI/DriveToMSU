6495cf1e89a5ac8a946d6795f156b503b1aa1984
Angelica Celestino <angelicacelestino@Angelicas-MacBook-Pro.local>
2020-11-25 18:42:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/drivetomsu/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.drivetomsu;
+
+import android.content.Intent;
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//intent to open Google maps
+    public void startImplicitIntent(View view) {
+
+        Intent newIntent = new Intent(Intent.ACTION_VIEW);
+        newIntent.setData(Uri.parse("geo:0,0?q=Montclair+State+University\n" + "    +New Jersey+USA"));
+        startActivity(newIntent);
+
+    }
+
+}
